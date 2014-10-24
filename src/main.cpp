43d260c0beebedb00ca5ff0af486b4f0357d2ad2
Michael Koval <mkoval@cs.cmu.edu>
2014-10-24 01:51:10 +0000

    updated mico_hardware

--- src/main.cpp
+++ src/main.cpp
@@ -21,21 +21,6 @@ int main(int argc, char* argv[])
   while (ros::ok())
   {
      robot.read();
-   //  robot.pos[1];
-   //     ROS_INFO_STREAM();
-   //  ROS_INFO_STREAM("robot position is:");
-     jaco_position_t pos = robot.returnPos();
-   //  ROS_INFO_STREAM(pos.joints[2]);
-   //  ROS_INFO_STREAM(pos.joints[3]);
-   //  ROS_INFO_STREAM(pos.joints[4]);
-   //  ROS_INFO_STREAM(pos.joints[6]);
-   //  ROS_INFO_STREAM(pos.joints[7]);
-     std_msgs::String msg;
-     std::stringstream ss;
-     ss << pos.joints[2];
-     msg.data = ss.str();
-    // ROS_INFO("%s", msg.data.c_str());
-     chatter_pub.publish(msg);
      cm.update(robot.get_time(), robot.get_period());
      robot.write();
      sleep(0.01);
