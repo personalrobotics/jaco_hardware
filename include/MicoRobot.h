d9bc7770385b3bac791e14002868ea1d1fe20686
Henny Admoni <hadmoni@users.noreply.github.com>
2017-02-07 15:34:45 -0500

    Update MicoRobot.h

--- include/MicoRobot.h
+++ include/MicoRobot.h
@@ -24,6 +24,7 @@
 
 using namespace std; // TODO: remove this and propogate change
 
+// TODO: the static consts should be in all caps
 static const double hardcoded_pos_midpoints[6] = { 0.0, -0.5 * M_PI, 0.5 * M_PI, 0.0, 0.0, 0.0 };
 static const int num_full_dof = 8;
 static const int num_arm_dof = 6;
